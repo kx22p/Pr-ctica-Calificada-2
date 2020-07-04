e6eebad96d108a78f865ccbd8766c8d0855168ef
kx22p <67158834+kx22p@users.noreply.github.com>
2020-07-03 21:16:47 -0500

    Create lagrange.py

---
+++ lagrange.py
@@ -0,0 +1,49 @@
+#INTERPOLACION DE LAGRANGE
+
+import numpy as np
+import sympy as sym
+import matplotlib.pyplot as plt
+
+#INGRESO DE DATOS
+
+xi = np.array([-3, -1, 2, 4])
+fi = np.array([0, 4, 3, 1])
+
+#PROCEDIMIENTO
+
+n = len(xi)
+x = sym.Symbol('x')
+polinomio = 0
+for i in range(0,n,1):
+	numerador = 1 
+	denominador = 1
+	for j in range(0,n,1):
+		if (i!=j):
+			numerador = numerador*(x-xi[j])
+			denominador = denominador*(xi[i]-xi[j])
+		termino = (numerador/denominador)*fi[i]
+	polinomio = polinomio + termino
+polisimple = sym.expand(polinomio)
+
+px = sym.lambdify(x,polinomio)
+
+#VECTORES PARA GRAFICAS
+
+muestras = 51
+a = np.min(xi)
+b = np.max(xi)
+p_xi = np.linspace(a,b,muestras)
+pfi = px(p_xi)
+
+#SALIDA
+
+print('polinomio: ')
+print(polinomio)
+print('polisimple: ')
+print(polisimple)
+
+#GRAFICA
+
+plt.plot(xi,fi,'o')
+plt.plot(p_xi,pfi)
+plt.show()
